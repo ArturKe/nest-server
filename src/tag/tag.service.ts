2f79e851882d910418980af0138da15e98e556c6
Artur <vashpartner@gmail.com>
2022-05-29 13:01:32 +0300

    initial commit

---
+++ src/tag/tag.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { TagEntity } from '@app/tag/tag.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class TagService {
+  constructor(
+    @InjectRepository(TagEntity)
+    private readonly tagRepository: Repository<TagEntity>,
+  ) {}
+  // findAll() {
+  //   return ['dragons', 'vagons', 'weather'];
+  // }
+  async findAll(): Promise<TagEntity[]> {
+    return await this.tagRepository.find();
+  }
+}
